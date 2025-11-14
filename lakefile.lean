f4e719ad93c667438dab5508587f18e9714000f4
Ben Breen <ben@axiomatic-ai.com>
2025-11-13 22:51:16 -0500

    lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -4,5 +4,9 @@ open Lake DSL
 package «qkd» where
   srcDir := "."
 
+@[default_target]
+lean_lib «QKD» where
+  srcDir := "."
+
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
